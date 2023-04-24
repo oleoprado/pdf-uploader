a67e1825700c2ed1b601db284caccc31184cc1c0
Leonardo Prado <leonardo.rprado@gmail.com>
2023-04-24 23:53:16 +0100

    feat: configuração do tema

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { ThemeConfig } from 'antd'
+
+export const theme: ThemeConfig = {
+  token: {
+    colorPrimary: '#FF6622',
+  },
+}
+// --primary: #FF6622;
+//   --secondary: #AF2500;
+//   --text-gray: #807B79;
+//   --white: #f2f2f2;
+//   --confirm: #0FC680;
