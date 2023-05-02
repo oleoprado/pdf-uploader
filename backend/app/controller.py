34348c32608db8f272511b7039102a495733bb1e
Leonardo Prado <leonardo.rprado@gmail.com>
2023-05-02 15:44:58 +0100

    feat: adiciona função de ler e salvar pdf

--- backend/app/controller.py
+++ backend/app/controller.py
@@ -1,6 +1,8 @@
+import tabula
+import tempfile
+import math
 from typing import Optional
 from schema import PDF_File, PDF_Content
-import math
 
 
 def save_pdf_file(pdf_name: str, pdf_file: bytes) -> int:
@@ -29,6 +31,23 @@ def save_pdf_content(
     )
 
 
+async def read_and_save_pdf(file):
+
+    with tempfile.NamedTemporaryFile(suffix=".pdf") as tmp:
+        tmp.write(await file.read())
+        file_path = tmp.name
+
+        pdf_name = file.filename
+        pdf_tables = tabula.read_pdf(file_path, pages="all")
+
+        pdf_id = save_pdf_file(pdf_name, open(file_path, "rb").read())
+
+        for _, table in enumerate(pdf_tables, start=1):
+            for _, row in table.iterrows():
+                item, description, unit, quantity = row.tolist()
+                save_pdf_content(item, description, unit, quantity, pdf_id)
+
+
 def search_description(query_params):
     query = (
         PDF_Content.select(
@@ -36,6 +55,7 @@ def search_description(query_params):
             PDF_Content.description,
             PDF_Content.unit,
             PDF_Content.quantity,
+            PDF_Content.pdf_id
         )
         .where(PDF_Content.description.contains(query_params))
         .join(PDF_File, on=(PDF_Content.pdf_id == PDF_File.id))
--- backend/app/database.py
+++
@@ -1,5 +0,0 @@
-def get_files():
-    return [
-        {"name": "Leonardo", "age": 32},
-        {"name": "Gabriel", "type": 28},
-    ]
