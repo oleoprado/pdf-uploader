637f30fb9dd86edcbf825192418e01fc6284f0ae
Leonardo Prado <leonardo.rprado@gmail.com>
2023-05-02 15:43:18 +0100

    feat: adiciona cors e ajusta rota post

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,40 +1,46 @@
-from fastapi import FastAPI, File, UploadFile
-# from typing import List, Union
-# from database import get_files
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
 from server import init_server
 from schema import create_tables
-from controller import search_description
+from controller import search_description, read_and_save_pdf
 
 
 create_tables()
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/files")
-async def save_file(file: UploadFile = File(...)):
-    # file_content = await file.read()
-    if not file:
-        return {"message": "No upload file sent"}
-    else:
-        return {"message": "Upload ok"}
+async def save_file(file: UploadFile):
+    try:
+        if not file:
+            return {"message": "No upload file sent"}
+        else:
+            await read_and_save_pdf(file)
+            return {"message": "Upload ok"}
+    except Exception as e:
+        print(f"Error processing file upload: {str(e)}")
+        return {"message": f"Error processing file upload: {str(e)}"}
 
 
 @app.get("/files")
 async def search(search: str):
-    print(search)
     result = search_description(search)
     return result
-
-
-# @app.get("/")
-# def read():
-#     return get_files()
-
-
-# @app.get("/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
 
 
 if __name__ == "__main__":
